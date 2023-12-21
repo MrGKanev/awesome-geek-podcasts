3962936bc72c02f5aff809124dd64b466b694ec1
Gabriel Kanev <contact@mrgkanev.eu>
2023-12-21 11:56:14 +0200

    Update available.py

--- available.py
+++ available.py
@@ -1,6 +1,8 @@
 import re
 import requests
 from tqdm import tqdm
+import git
+from datetime import datetime
 
 def get_urls_from_readme(file_path):
     with open(file_path, 'r') as file:
@@ -32,6 +34,23 @@ def check_urls(urls):
                 pbar.update(1)
     return invalid_urls
 
+def remove_invalid_urls(file_path, invalid_urls):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(file_path, 'w') as file:
+        for line in lines:
+            if not any(url in line for url in invalid_urls):
+                file.write(line)
+
+def create_branch_and_commit(commit_message):
+    repo = git.Repo(search_parent_directories=True)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    branch_name = f"invalid_urls_{timestamp}"
+    repo.git.checkout('-b', branch_name)
+    repo.git.add(update=True)
+    repo.index.commit(commit_message)
+
 def main():
     file_path = 'readme.md'  # Replace this with the path to your readme file
     urls = get_urls_from_readme(file_path)
@@ -42,6 +61,8 @@ def main():
         print("The following URLs did not return a 200 status code. Check 'url_check_logs.txt' for details:")
         for url in invalid_urls:
             print(url)
+        remove_invalid_urls(file_path, invalid_urls)
+        create_branch_and_commit('Removed invalid URLs from readme.md')
     else:
         print("All URLs in the readme.md file returned a 200 status code.")
 
